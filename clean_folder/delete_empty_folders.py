be83586897377986de599079765925f5cce2717e
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-08-08 20:27:22 +0300

    Create delete_empty_folders.py

---
+++ clean_folder/delete_empty_folders.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+def delete_empty_folder(path_to_folder: str):
+    path = Path(path_to_folder)
+    list_name = ["archives", "video", "audio", "documents", "images", "Previously unpacked archives"]
+
+    for item in path.iterdir():
+        if item.is_dir():
+            delete_empty_folder(item)
+
+    if not any(path.iterdir()) and path.name not in list_name:        
+        path.rmdir()
+        
