c82bd0c7e44eba331c952c8f02e2244a3b18c073
OlhaVerbova <137227019+OlhaVerbova@users.noreply.github.com>
2023-08-08 20:27:15 +0300

    Create normalize.py

---
+++ clean_folder/normalize.py
@@ -0,0 +1,16 @@
+import re
+def normalize(text: str)-> str:    
+    CYRILLIC_SYMBOLS = "абвгдеёжзийклмнопрстуфхцчшщъыьэюяєіїґ"
+    TRANSLATION = ("a", "b", "v", "g", "d", "e", "e", "j", "z", "i", "j", "k", "l", "m", "n", "o", "p", "r", "s", "t", "u",
+               "f", "h", "ts", "ch", "sh", "sch", "", "y", "", "e", "yu", "ya", "je", "i", "ji", "g")
+    TRANS = {}  
+    
+    mini_cy = tuple(CYRILLIC_SYMBOLS)
+    for c, l in zip(mini_cy, TRANSLATION):
+        TRANS[ord(c)] = l
+        TRANS[ord(c.upper())] = l.upper()
+    temp_name = text.translate(TRANS)
+    pat = r"[^\w\d]+"
+    result_name = re.sub(pat, "_", temp_name)
+    
+    return result_name
